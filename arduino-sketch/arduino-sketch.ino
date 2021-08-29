62bae7e05ec23642a01630e63148294165d7a8da
David Cabiness <dcabines@gmail.com>
2021-08-29 15:02:38 -0400

    added bpm calc from example

--- arduino-sketch/arduino-sketch.ino
+++ arduino-sketch/arduino-sketch.ino
@@ -1,8 +1,16 @@
 #include <Wire.h>
 #include "MAX30105.h"
+#include "heartRate.h"
 // https://github.com/sparkfun/SparkFun_MAX3010x_Sensor_Library
 
 MAX30105 sensor;
+
+const byte RATE_SIZE = 4;
+byte rates[RATE_SIZE];
+byte rateSpot = 0;
+long lastBeat = 0;
+float beatsPerMinute;
+int beatAvg;
 
 void setup()
 {
@@ -15,10 +23,35 @@ void setup()
   }
 
   sensor.setup();
+  sensor.setPulseAmplitudeRed(0x0A);
+  sensor.setPulseAmplitudeGreen(0);
 }
 
 void loop()
 {
+  long irValue = sensor.getIR();
+
+  if (checkForBeat(irValue) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute;
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+  }
+  
   Serial.print("{ ");
 
   Serial.print("\"red\":");
@@ -26,11 +59,23 @@ void loop()
   Serial.print(", ");
 
   Serial.print("\"ir\":");
-  Serial.print(sensor.getIR());
+  Serial.print(irValue);
   Serial.print(", ");
 
   Serial.print("\"green\":");
   Serial.print(sensor.getGreen());
+  Serial.print(", ");
+
+  Serial.print("\"beat\":");
+  Serial.print(checkForBeat(irValue));
+  Serial.print(", ");
+
+  Serial.print("\"bpm\":");
+  Serial.print(beatsPerMinute);
+  Serial.print(", ");
+
+  Serial.print("\"beatAvg\":");
+  Serial.print(beatAvg);
 
   Serial.print(" }");
 
